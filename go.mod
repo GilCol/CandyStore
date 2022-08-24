0e7697b217aa81f059ab07ec7ffd0748973002b7
Erik Gil Forsman <erik.gil.forsman@gmail.com>
2022-08-24 11:57:11 +0200

    Adds network request, sorting and json export

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module candystore
 
 go 1.19
+
+require (
+	github.com/PuerkitoBio/goquery v1.8.0 // indirect
+	github.com/andybalholm/cascadia v1.3.1 // indirect
+	golang.org/x/net v0.0.0-20220822230855-b0a4917ee28c // indirect
+)
--- main/main.go
+++ main/main.go
@@ -1,7 +1,69 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"sort"
+	"strconv"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+type Customer struct {
+	Name           string
+	FavouriteSnack string
+	TotalSnacks    int
+}
 
 func main() {
-	fmt.Println("Hello world")
+	webPage := "https://candystore.zimpler.net/#candystore-customers"
+
+	res, _ := http.Get(webPage)
+	doc, _ := goquery.NewDocumentFromReader(res.Body)
+
+	var topCustomers []Customer
+
+	// Scrape expected data!
+	doc.Find(".summary").First().Find("tbody").First().Find("tr").Each(func(i int, s *goquery.Selection) {
+		attrTotalCandy, exists := s.Children().First().Attr("x-total-candy")
+
+		if exists {
+			name := s.Children().First().Text()
+			totalSnacksInt, _ := strconv.Atoi(attrTotalCandy)
+			favSnacks := s.Children().Last().Text()
+
+			c := Customer{
+				Name:           name,
+				TotalSnacks:    totalSnacksInt,
+				FavouriteSnack: favSnacks,
+			}
+
+			topCustomers = append(topCustomers, c)
+		}
+	})
+
+	sortCustomers(topCustomers)
+	jsn, err := transformtoJson(topCustomers)
+
+	if err == nil {
+		fmt.Println(string(jsn))
+	} else {
+		log.Panic("Failed to transform to json")
+	}
+}
+
+func sortCustomers(topCustomers []Customer) {
+	sort.Slice(topCustomers, func(i, j int) bool {
+		return topCustomers[i].TotalSnacks > topCustomers[j].TotalSnacks
+	})
+}
+
+func transformtoJson(topCustomers []Customer) ([]byte, error) {
+	if jsn, err := json.Marshal(topCustomers); err == nil {
+		return jsn, nil
+	} else {
+		return nil, err
+	}
 }
